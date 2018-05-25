e90ffc5c689bdacdd11edc71be0c09c83d54c27c
Gabriel Pinheiro <pinheiro.gbr@gmail.com>
2018-05-24 23:20:21 -0300

    New info for the bot

--- mainBot.py
+++ mainBot.py
@@ -1,12 +1,8 @@
 import discord
 from discord.ext import commands
-import random
 import plugins
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-
-There are a number of utility commands being showcased here.'''
+description = '''MidnightBot Several uses for no reason'''
 bot = commands.Bot(command_prefix='!!', description=description)
 
 @bot.event
@@ -16,51 +12,43 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
 
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
+# @bot.command()
+# async def roll(dice : str):
+#     """Rolls a dice in NdN format."""
+#     try:
+#         rolls, limit = map(int, dice.split('d'))
+#     except Exception:
+#         await bot.say('Format has to be in NdN!')
+#         return
 
-@bot.command(description='For when you wanna settle the score some other way')
+#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+#     await bot.say(result)
+
+@bot.command(description='Help you to decide your life')
 async def choose(*choices : str):
-    """Chooses between multiple choices."""
+    """Decide your life"""
     await bot.say(random.choice(choices))
 
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
 
 @bot.command()
 async def joined(member : discord.Member):
     """Alerts when a member enter the server"""
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+    await bot.say('{0.name} invaded {0.joined_at}'.format(member))
 
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
+# @bot.command()
+# async def music(*)
 
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+@bot.command()
+async def funcname(phrase : str):
+    echo.print
 
 @cool.command(name='bot')
 async def _bot():
     """Is the bot cool?"""
     await bot.say('Yes, the bot is cool.')
+
+
 
 bot.run('NDQ4NjMzMjQxMzk0NzQxMjQ4.DeY_ow.SktFlzgpKzyMXTo4a0l5XfAa6QA')
